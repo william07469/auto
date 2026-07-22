f2dfc296479a953a951076a19a354ba550873344
william07469 <clnkbrt127@gmail.com>
2026-07-22 13:38:35 +0300

    add TikTok and fix Instagram link in contact

--- src/components/site/Contact.tsx
+++ src/components/site/Contact.tsx
@@ -4,6 +4,14 @@ import { Phone, Mail, MapPin, Instagram, MessageCircle, Clock, Send } from "luci
 import { toast } from "sonner";
 import { supabase } from "@/integrations/client";
 
+function TikTokIcon({ className }: { className?: string }) {
+  return (
+    <svg className={className} viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+      <path d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-2.88 2.5 2.89 2.89 0 0 1-2.89-2.89 2.89 2.89 0 0 1 2.89-2.89c.28 0 .54.04.79.1V9.01a6.33 6.33 0 0 0-.79-.05 6.34 6.34 0 0 0-6.34 6.34 6.34 6.34 0 0 0 6.34 6.34 6.34 6.34 0 0 0 6.33-6.34V8.69a8.18 8.18 0 0 0 4.78 1.52V6.75a4.85 4.85 0 0 1-1.01-.06z" />
+    </svg>
+  );
+}
+
 const contactLinks = [
   {
     icon: Phone,
@@ -31,7 +39,14 @@ const contactLinks = [
     label: "Instagram",
     value: "@wv_detailing",
     sub: "Aktuelle Projekte",
-    href: "https://instagram.com",
+    href: "https://instagram.com/wv_detailing",
+  },
+  {
+    icon: TikTokIcon,
+    label: "TikTok",
+    value: "@wv.detailing",
+    sub: "Videos & Reels",
+    href: "https://tiktok.com/@wv.detailing",
   },
 ];
 
